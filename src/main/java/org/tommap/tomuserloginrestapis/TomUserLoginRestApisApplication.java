100282a7ef9d1adcd9e3a4e31d20e0fb20b04b8d
Khanh Le <tommapcoder@gmail.com>
2025-08-27 14:00:03 +0700

    init user login rest service

---
+++ src/main/java/org/tommap/tomuserloginrestapis/TomUserLoginRestApisApplication.java
@@ -0,0 +1,13 @@
+package org.tommap.tomuserloginrestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomUserLoginRestApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomUserLoginRestApisApplication.class, args);
+    }
+
+}
