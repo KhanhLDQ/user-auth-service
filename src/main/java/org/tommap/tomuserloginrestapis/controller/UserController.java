53bbbffead4c36fd36c932d996bac3b48012652f
Khanh Le <tommapcoder@gmail.com>
2025-09-03 10:57:18 +0700

    setup user controller

---
+++ src/main/java/org/tommap/tomuserloginrestapis/controller/UserController.java
@@ -0,0 +1,32 @@
+package org.tommap.tomuserloginrestapis.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @GetMapping
+    public String getUserDetails() {
+        return "get user details was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
+}
